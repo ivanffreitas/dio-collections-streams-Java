31a7803b3b42cda02ac490441d49f26734689b1b
ivanffreitas <ivanff.ti@gmail.com>
2021-12-02 12:16:06 -0300

    exemplos de Optionals

---
+++ src/main/java/dio/ivanilson/optionals/ExemploOptionalPrimitivos.java
@@ -0,0 +1,21 @@
+package dio.ivanilson.optionals;
+
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+import java.util.OptionalLong;
+
+public class ExemploOptionalPrimitivos {
+
+    public static void main(String[] args) {
+
+        System.out.println("---Valor inteiro opcional---");
+        OptionalInt.of(12).ifPresent(System.out::println);
+
+        System.out.println("---Valor decimal opcional---");
+        OptionalDouble.of(55.0).ifPresent(System.out::println);
+
+        System.out.println("---Valor longo opcinal---");
+        OptionalLong.of(23L).ifPresent(System.out::println);
+
+    }
+}
